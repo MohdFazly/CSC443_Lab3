b896dda5f83bb54390aa9838c3d634627998d97e
MohdFazly <2020819344@student.uitm.edu.my>
2022-05-22 15:04:50 +0800

    fff

--- variable.php
+++ variable.php
@@ -8,9 +8,15 @@
 </head>
 <body>
     <?php
-$a = "PHP";
-$a = $a + 1;
-echo $a;
+$$num == 22 % 2;
+if ($num = 2 || $num = 3 || $num = 4){
+ 	echo "This is not the number "+" you're looking for";
+	}
+else if ($num = 5){
+echo "A snack is on its way to you!";
+}
+else
+echo "Ahaaaa Yes!";
     ?>
 </body>
 </html>
